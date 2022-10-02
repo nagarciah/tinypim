f587954ae07ba7d11f2bceba1a40b79341bfc01c
nagarciah@yahoo.com <nelson.garcia@verivox.com>
2022-10-02 13:41:57 +0200

    Initial scaffolding

---
+++ api/src/main/java/com/nagarciah/tinypim/TinypimApplication.java
@@ -0,0 +1,13 @@
+package com.nagarciah.tinypim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinypimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinypimApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/nagarciah/tinypim/TinypimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarciah.tinypim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinypimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
